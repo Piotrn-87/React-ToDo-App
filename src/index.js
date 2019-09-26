9e2c0aea67012e712165c4cd9beaecbad02d207c
Piotr Napieralski <piotrn-87@o2.pl>
2019-09-26 20:00:41 +0200

    Fix: Change some feature in js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import MyApp from './MyApp'
-import './MyApp.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import MyApp from "./MyApp";
+import "./MyApp.css";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(< MyApp />, root);
+ReactDOM.render(<MyApp />, root);
