2b5cd15305f32dea9e4f4c944fa8c1794474fe32
Piotr Napieralski <piotrn-87@o2.pl>
2019-11-19 19:32:29 +0100

    Updates

---
+++ precache-manifest.93b5947fb359651918d2b8dc5e31c9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad30e8051e54c57f458cd55fadbd34a8",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "96156951d7d48dc32675",
+    "url": "/React-ToDo-App/static/css/main.40a70eaa.chunk.css"
+  },
+  {
+    "revision": "bf3346bcaafb7f8f7e06",
+    "url": "/React-ToDo-App/static/js/2.a4ce696e.chunk.js"
+  },
+  {
+    "revision": "96156951d7d48dc32675",
+    "url": "/React-ToDo-App/static/js/main.32674fb2.chunk.js"
+  },
+  {
+    "revision": "0dcaf40595a6f81f0dab",
+    "url": "/React-ToDo-App/static/js/runtime~main.06e71d61.js"
+  }
+]);
